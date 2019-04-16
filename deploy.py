bad7cabfc0ab7e738ca8e7a841a382a052e9b7c6
Hudson784 <hudson_adams@hotmail.com>
2019-04-16 17:20:59 -0400

    Test

--- deploy.py
+++ deploy.py
@@ -1,4 +1,4 @@
-from flask import Flask 
+from flask import Flask, jsonify
 import json
 from flask import render_template 
 #Import the necessary methods from tweepy library
@@ -8,9 +8,11 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     tweets_data = []
+    tweet = ''
     tweets_data_path = 'twitter_data.txt.'
     tweets_file = open(tweets_data_path, "r")
     for line in tweets_file:
-        tweet = json.loads(line)
+        tweet += line
         tweets_data.append(tweet)
-    return render_template('home.html', records = tweets_data)
+    return jsonify(tweet) 
+    #render_template('home.html', records = tweets_data)
