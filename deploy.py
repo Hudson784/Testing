ead0224744af6c756ff4dabcef9fdca59a72f83e
Hudson784 <hudson_adams@hotmail.com>
2019-04-16 15:48:03 -0400

    testing

--- deploy.py
+++ deploy.py
@@ -1,8 +1,26 @@
 from flask import Flask 
+import json
+#Import the necessary methods from tweepy library
+from tweepy.streaming import StreamListener
+from tweepy import OAuthHandler
+from tweepy import Stream
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index(): 
-    return '<h1> Web Project Members </h1> <br <h1> Hudson, Chantel, Kmani, Maya </h1>'
+    tweets_data_path = 'twitter_data.txt.'
+    tweets_file = open(tweets_data_path, "r")
+for line in tweets_file:
+    tweet = json.loads(line)
+    test = tweet['id']
+    test.append(tweet['text'])
+    test.append( user['name']))
+    print("First line of data: \n")
+    print("Tweet ID: ", tweet['id'])
+    print("Tweet: ", tweet['text'])
+    user = tweet['user']
+    print("Name: ", user['name'])
+    print("Screen Name: ", user['screen_name'])
+    return jsonify(test)
