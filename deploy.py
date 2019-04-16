644a36b75f6d1cbd4b96b8d0ebca2a056b4630f7
Hudson784 <hudson_adams@hotmail.com>
2019-04-16 15:52:51 -0400

    testing

--- deploy.py
+++ deploy.py
@@ -10,15 +10,17 @@ app = Flask(__name__)
 
 @app.route('/')
 def index(): 
+    tweets_data = []
     tweets_data_path = 'twitter_data.txt.'
     tweets_file = open(tweets_data_path, "r")
-for line in tweets_file:
-    tweet = json.loads(line)
-    test = tweet['id']
-    print("First line of data: \n")
-    print("Tweet ID: ", tweet['id'])
-    print("Tweet: ", tweet['text'])
-    user = tweet['user']
-    print("Name: ", user['name'])
-    print("Screen Name: ", user['screen_name'])
+    for line in tweets_file:
+        tweet = json.loads(line)
+        test = tweet['id']
+        print("First line of data: \n")
+        print("Tweet ID: ", tweet['id'])
+        print("Tweet: ", tweet['text'])
+        user = tweet['user']
+        print("Name: ", user['name'])
+        print("Screen Name: ", user['screen_name']
+        tweets_data.append(tweet)
     return jsonify(test)
